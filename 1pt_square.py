58f9e941c5510dbf91aab0c4d49151e4aff4cf19
machi_syo <104885577+machistore@users.noreply.github.com>
2023-04-05 12:45:55 +0900

    Create 1pt_square.py

---
+++ 1pt_square.py
@@ -0,0 +1,9 @@
+#Created by Katsutoshi Machida Apr. 2023.
+import vs #モジュールの呼び出し
+
+orgin = (0, 0) #原点(x, y) (変数定義)
+direction = (1, 0) #傾き(x, y) (変数定義)
+width = vs.Px #オブジェクト情報パレットで指定された幅 (変数定義)
+height = vs.Py #オブジェクト情報パレットで指定された高さ(奥行き) (変数定義)
+
+vs.RectangleN(orgin, direction, width, height) #2D四角形オブジェクトの生成API
