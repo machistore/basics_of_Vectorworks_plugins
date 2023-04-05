50fe4f6ebffa2ebdd5259f273fe506059ef967ca
machi_syo <104885577+machistore@users.noreply.github.com>
2023-04-05 14:08:59 +0900

    Create 2pt_square.py

---
+++ 2pt_square.py
@@ -0,0 +1,21 @@
+#Created by Katsutoshi Machida Apr. 2023.
+
+import vs
+
+width =  vs.PLineLength
+depth =  vs.PLineLength
+
+x = 0
+y = 0
+
+vs.Rect(x, y, width, depth) 
+
+#パラメータ
+# |#|名前      |フィールド名|型       |初期値|
+# |---|---|---|---|---|
+# |1|LineLength|辺の長さ   |Dimension|1     |
+
+
+#Features
+#"2点型オブジェクト"を使用して四角形オブジェクトを生成するプラグインオブジェクト
+#2点型オブジェクトの作成方法を理解するためにシンプルにつくってみたプラグインオブジェクトです.
