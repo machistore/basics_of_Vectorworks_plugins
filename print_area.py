f875ec5b4b09abcb6b0e992edf48de268df220e5
machi_syo <104885577+machistore@users.noreply.github.com>
2023-05-03 09:34:09 +0900

    Create print_area.py

---
+++ print_area.py
@@ -0,0 +1,9 @@
+
+import vs
+
+target = vs.FSActLayer() #選択図形の中で一番上の図形をtargetとする
+area = round(vs.ObjAreaN(target),2) #選択した図形の面積をareaとする
+cent = vs.HCenter(target)  #targetの中心をcentとする
+
+vs.MoveTo(cent) #選択した図形のセンター座標にハンドルを移動
+vs.CreateText(str(area)+"m2") #面積の文字列オブジェクトを作成
