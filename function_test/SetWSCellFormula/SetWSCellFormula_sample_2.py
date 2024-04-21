681f823bd9fc209a4d8f90bdf87daf3dcfeb9e69
machi_syo <104885577+machistore@users.noreply.github.com>
2024-04-21 14:14:34 +0900

    Update SetWSCellFormula_sample_2.py

--- function_test/SetWSCellFormula/SetWSCellFormula_sample_2.py
+++ function_test/SetWSCellFormula/SetWSCellFormula_sample_2.py
@@ -1,19 +1,29 @@
-# description: ワークシートのセルに数式や数値、文字列などを設定したり、条件式を設定したりするサンプルスクリプト
-# author: Katsutoshi Machida
+# Description: ワークシートにヘッダーを設定してデータを設定するサンプル
+# Author: Katsutoshi Machida
 # created: 2024-04-21
 # last update: 2024-04-21
-# app_version: Vectorworks 2024
-# os_version: Windows 11 home
-# language: Python
+# Apps that have been confirmed to work: Vectorworks 2024
+# os: Windows 11 home (version 23H2) 
 
 
 import vs
 
-ws_name = 'Sample Worksheet' # ワークシート名
-ws_handle = vs.CreateWS(ws_name, 10, 10)  # 10行10列のワークシート
+def create_worksheet_with_headers(): # ワークシートにヘッダーを設定してデータを設定する関数
+    # ワークシートを作成
+    ws_name = 'Sample_Worksheet_2'
+    # すでに同じ名前のワークシートが存在するかチェックし、存在する場合はそのハンドルを取得
+    ws_handle = vs.CreateWS(ws_name, 2, 2)  # 2行2列のワークシート
 
-vs.SetWSCellFormula(ws_handle,4,2,4,2,'=3*2') # セルに数式を設定
-vs.SetWSCellFormula(ws_handle,1,1,2,10,'<empty>') # セルに空白を設定
-vs.SetWSCellFormula(ws_handle,3,0,3,0,"=DATABASE(L='レイヤ-1')") # セルにデータベース関数を設定 
+    # ヘッダーを設定
+    vs.SetWSCellFormula(ws_handle, 1, 1, 1, 1, 'ID')  # 1行1列目に"ID"という文字列
+    vs.SetWSCellFormula(ws_handle, 1, 2, 1, 2, 'NAME')  # 1行2列目に"NAME"という文字列
 
-vs.ShowWS(ws_handle, True) # ワークシートを表示
+    # サンプルデータを設定
+    vs.SetWSCellFormula(ws_handle, 2, 1, 2, 1, '1')  # 2行1列目に数値1
+    vs.SetWSCellFormula(ws_handle, 2, 2, 2, 2, '\'ippan\'')  # 2行2列目に"ippan"という文字列
+
+    # ワークシートを表示
+    vs.ShowWS(ws_handle, True)
+
+# 関数を実行してワークシートを生成
+create_worksheet_with_headers()
